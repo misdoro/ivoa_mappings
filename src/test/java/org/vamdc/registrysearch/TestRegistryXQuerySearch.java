2b78b8be00fbb4f6d1dc296e4b03ee4e7e46d40c
Misha Doronin <misha@doronin.org>
2011-12-02 20:46:08 +0100

    More playing with XQuery

--- src/test/java/org/vamdc/registrysearch/TestRegistryXQuerySearch.java
+++ src/test/java/org/vamdc/registrysearch/TestRegistryXQuerySearch.java
@@ -13,28 +13,31 @@ import net.ivoa.xml.voresource.v1.Capability;
 import net.ivoa.xml.voresource.v1.Service;
 import junit.framework.TestCase;
 
+import com.sun.org.apache.xerces.internal.dom.ElementNSImpl;
+
 public class TestRegistryXQuerySearch extends TestCase {
 
-	/*private static String xquery  = "declare namespace ri='http://www.ivoa.net/xml/RegistryInterface/v1.0'; " + 
-	        "for $x in //ri:Resource " + 
-	        "where $x/capability[@standardID='ivo://vamdc/std/VAMDC-TAP']" +
-	        "and $x/@status='active' " +
-	        "return $x";
-	*/
+
 	private static String xquery  = "declare namespace ri='http://www.ivoa.net/xml/RegistryInterface/v1.0'; " + 
-	        "for $x in //ri:Resource " + 
-	        "where $x/capability[@standardID='ivo://vamdc/std/VAMDC-TAP']" +
-	        "and $x/@status='active' " +
-	        "return $x";
-	
+			"for $x in //ri:Resource " + 
+			"where $x/capability[@standardID='ivo://vamdc/std/VAMDC-TAP']" +
+			"and $x/@status='active' " +
+			"return $x";
+
+	private static String xquery2  = "declare namespace ri='http://www.ivoa.net/xml/RegistryInterface/v1.0'; " + 
+			"for $x in //ri:Resource " + 
+			"where $x/capability[@standardID='ivo://vamdc/std/VAMDC-TAP']" +
+			"and $x/@status='active' " +
+			"return $x/capability";
+
+
 	public void testXQuery() {
-		
+
 		XQuerySearch xQuerySearch = new XQuerySearch();
 		xQuerySearch.setXquery(xquery);
-		
+
 		RegistrySearchPortType search = RegistryFactory.getSearchPort();
-		
-		xQuerySearch.setXquery(xquery);
+
 		try {
 			XQuerySearchResponse xqResp = search.xQuerySearch(xQuerySearch);
 			List<Object> searchResult = xqResp.getAny();
@@ -49,11 +52,43 @@ public class TestRegistryXQuerySearch extends TestCase {
 				for (Capability cap:srv.getCapability()){
 					System.out.println(cap);
 				}
-				
+
 				System.out.println(obj.getValue().getClass());
 				System.out.println("SearchResult:"+element.toString());
 			}
-			
+
+		} catch (ErrorResp e) {
+			fail (e.getMessage());
+		} catch (OpUnsupportedResp e) {
+			fail (e.getMessage());
+		} catch (Exception e){
+			e.printStackTrace();
+			fail (e.getMessage());
+		}
+	}
+
+	public void testXQueryCapability() {
+
+		XQuerySearch xQuerySearch = new XQuerySearch();
+		xQuerySearch.setXquery(xquery2);
+
+		RegistrySearchPortType search = RegistryFactory.getSearchPort();
+
+		try {
+			XQuerySearchResponse xqResp = search.xQuerySearch(xQuerySearch);
+			List<Object> searchResult = xqResp.getAny();
+			assertNotNull(searchResult);
+			assertTrue(searchResult.size()>0);
+			for (Object element:searchResult){
+				System.out.println();
+				com.sun.org.apache.xerces.internal.dom.ElementNSImpl el = (ElementNSImpl) element;
+				
+				//System.out.println(el.getElementsByTagName(""));
+				System.out.println(el.getChildNodes().getLength());
+				
+				System.out.println("SearchResult:"+element.toString());
+			}
+
 		} catch (ErrorResp e) {
 			fail (e.getMessage());
 		} catch (OpUnsupportedResp e) {
